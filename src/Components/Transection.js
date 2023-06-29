56a3564774cb0637c19f65c80a5d27f37369d868
Thanawat Jaimoi <tong3089@gmail.com>
2023-06-30 01:21:37 +0700

    add propstype

--- src/Components/Transection.js
+++ src/Components/Transection.js
@@ -1,20 +1,21 @@
 import Item from "./Item";
 import './Transection.css'
+import { v4 as uuidv4 } from 'uuid';
 
 const Transection = () => {
     const data = [
-        {id:1, title: "ค่ารักษาพยาบาล", amount: 6000},
-        {id:2, title: "ค่าน้ำมัน", amount: 5000},
-        {id:3, title: "ค่าเช่าบ้าน", amount: 8000},
-        {id:4, title: "เงินเดือน", amount: 100000},
-        {id:5, title: "ค่ารถ", amount: 12000},
-        {id:6, title: "ค่าเดินทาง", amount: 3000},
+        {title: "ค่ารักษาพยาบาล", amount: 6000},
+        {title: "ค่าน้ำมัน", amount: 5000},
+        {title: "ค่าเช่าบ้าน", amount: 8000},
+        {title: "เงินเดือน", amount: 100000},
+        {title: "ค่างวดรถ", amount: 12000},
+        {title: "ค่าเดินทาง", amount: 3000},
     ]
     return (
       <ul className="item-list">
         {data.map((element) => {
             // return <Item title={element.title} amount={element.amount}/>
-            return <Item {...element} key = {element.id}/>
+            return <Item {...element} key={uuidv4()}/>
         })}
         </ul>
     );
